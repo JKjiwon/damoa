f89add5e04b76523ce4010937870e4eaf33f04d4
kimjiwon <jwkim9205@gmail.com>
2021-05-18 11:32:19 +0900

    first commit

---
+++ src/main/java/hello/sns/SnsApplication.java
@@ -0,0 +1,13 @@
+package hello.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsApplication.class, args);
+	}
+
+}
