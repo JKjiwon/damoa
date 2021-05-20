01199ad5db40b1ea21909be2b0d07155adaf2d3c
kimjiwon <jwkim9205@gmail.com>
2021-05-21 01:47:13 +0900

    Remove 불필요한 import 제거

--- src/main/java/hello/sns/domain/post/Like.java
+++ src/main/java/hello/sns/domain/post/Like.java
@@ -4,7 +4,6 @@ import static javax.persistence.FetchType.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
