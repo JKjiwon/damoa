1dbfb28e27d14226c7c555368c2e73ce827da76e
kimjiwon <jwkim.oa@gmail.com>
2021-06-29 21:53:51 +0900

    Add: FileController - extractFilePath: URL로부터 파일이름 추출

--- src/main/java/com/damoa/web/controller/FileController.java
+++ src/main/java/com/damoa/web/controller/FileController.java
@@ -1,13 +1,16 @@
 package com.damoa.web.controller;
 
 import com.damoa.service.FileService;
+import com.damoa.util.FileUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.MalformedURLException;
 
 @RestController
@@ -16,8 +19,18 @@ public class FileController {
 
     private final FileService fileService;
 
-    @GetMapping("/images/{filename}")
-    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+    @GetMapping("/api/images/**")
+    public Resource downloadImage(HttpServletRequest request) throws MalformedURLException {
+        String filename = extractFilePath(request);
         return new UrlResource("file:" + fileService.getFullPath(filename));
+    }
+
+    private String extractFilePath(HttpServletRequest request) {
+        String path = (String) request.getAttribute(
+                HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+        String bestMatchPattern = (String) request.getAttribute(
+                HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        AntPathMatcher apm = new AntPathMatcher();
+        return apm.extractPathWithinPattern(bestMatchPattern, path);
     }
 }
